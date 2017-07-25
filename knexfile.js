a64f54c81108cc9a20a25176fbe699b22f11dd72
Alex Read <alexread730@gmail.com>
2017-07-25 11:54:01 -0600

    added env

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,15 @@
 // Update with your config settings.
+const dotenv = require('dotenv').config;
 
 module.exports = {
 
   development: {
     client: 'pg',
     connection: 'postgres://localhost/events'
+  },
+    production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
   }
 
 };
